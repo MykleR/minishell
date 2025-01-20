ad7e0edf8c9d5a1ea63db4c04b6bc91c50b31344
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-01-20 16:29:38 +0100

    Proto Exec & Pipe (v1)

--- headers/proto.h
+++ headers/proto.h
@@ -6,12 +6,18 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:00:04 by thomarna          #+#    #+#             */
-/*   Updated: 2025/01/17 16:46:11 by thomarna         ###   ########.fr       */
+/*   Updated: 2025/01/20 16:07:49 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROTO_H
 # define PROTO_H
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
 typedef struct s_env
 {
@@ -19,5 +25,12 @@ typedef struct s_env
 	char			*value;
 	struct s_env	*next;
 }					t_env;
+
+typedef struct s_pipe
+{
+	char **env;
+	int	size;
+	int	outfile;
+}	t_pipe;
 
 #endif
