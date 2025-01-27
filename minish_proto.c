d6856493de046ed4b0c76f5d1fe41329c5114a8e
Michael Rouves <mrouves@student.42angouleme.fr>
2025-01-27 21:15:51 +0100

    lexer without quotes

--- minish_proto.c
+++ minish_proto.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lexer_proto.c                                      :+:      :+:    :+:   */
+/*   minish_proto.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 14:45:29 by mrouves           #+#    #+#             */
-/*   Updated: 2025/01/22 20:36:14 by mrouves          ###   ########.fr       */
+/*   Updated: 2025/01/27 21:14:50 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,130 +16,135 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-#define NB_TOKENS 14
+#define TOKENS_NB 11
+#define TOKENS_STR {"||", "&&", "<<", ">>", "<", ">", "|", "'", "\"", "(", ")"}
 
 typedef enum e_token_type
 {
-	T_WORD,
-	T_SEMI,
-	T_PIPE,
 	T_OR,
 	T_AND,
+	T_DLESS,
+	T_DGREAT,
+	T_LESS,
+	T_GREAT,
+	T_PIPE,
 	T_SQUOTE,
 	T_DQUOTE,
 	T_LBRACE,
 	T_RBRACE,
-	T_LESS,
-	T_DLESS,
-	T_GREAT,
-	T_DGREAT,
-	T_EOF,
+	T_WORD,
 }	t_token_type;
+
+typedef enum e_quote_type
+{
+	Q_NONE,
+	Q_SINGLE,
+	Q_DOUBLE,
+}	t_quote_type;
 
 typedef struct s_token
 {
-	t_collection	str;
+	size_t			start;
+	size_t			len;
 	t_token_type	type;
+	t_quote_type	ctx;
 }	t_token;
 
 typedef struct s_lexer
 {
-	t_collection	tokens;
-	size_t			index;
-	t_token_type	ctx;
+	size_t			i;
+	t_quote_type	quote;
+	t_token_type	token;
 }	t_lexer;
 
 typedef struct s_shell
 {
 	const char		*cmd;
-	t_lexer			lexer;
-	t_hm			str_tokens;
+	t_collection	tokens;
 }	t_shell;
-
-bool	token_init(t_token *token, t_token_type t)
-{
-	static t_clear_info	clear = {0};
-	
-	if (__builtin_expect(!token, 0))
-		return (false);
-	token->type = t;
-	return (collection_create(&token->str, 32, sizeof(char),clear));
-}
-
-void	token_destroy(t_token *token)
-{
-	if (__builtin_expect(token != NULL, 1))
-		collection_destroy(&token->str);
-}
-
-bool	lexer_init(t_lexer *lexer)
-{
-	static const t_clear_info	clear = {
-		(void (*)(void*))token_destroy, T_STACK};
-
-	if (__builtin_expect(!lexer, 0))
-		return (false);
-	lexer->ctx = 0;
-	lexer->index = 0;
-	return (collection_create(&lexer->tokens, 32, sizeof(t_token), clear));
-}
-
-void	lexer_destroy(t_lexer *lexer)
-{
-	if (__builtin_expect(lexer != NULL, 1))
-		collection_destroy(&lexer->tokens);
-}
 
 bool	shell_init(t_shell *shell, const char *cmd)
 {
 	static const t_clear_info	clear = {0};
-	static const char			*strs[NB_TOKENS] = {"\\", ";", "|", "||", "&&",
-		"'", "\"", "(", ")", "<", "<<", ">", ">>"};
-	t_token_type				type;
-	bool						success;
-
-
+	
 	if (__builtin_expect(!shell, 0))
 		return (false);
 	shell->cmd = ft_strdup(cmd);
-	success = shell->cmd && lexer_init(&shell->lexer)
-		&& hm_create(&shell->str_tokens, 32, sizeof(t_token_type), clear);
-	type = -1;
-	while (success && ++type < NB_TOKENS)
-		hm_set(&shell->str_tokens, strs[type], &((t_token_type){type}));
-	return (success);
+	return (shell->cmd && collection_create(
+		&shell->tokens, 32, sizeof(t_token), clear));
 }
 
 void	shell_destroy(t_shell *shell)
 {
 	if (__builtin_expect(!shell, 0))
 		return ;
-	lexer_destroy(&shell->lexer);
+	collection_destroy(&shell->tokens);
 	alloc_f((void *)shell->cmd);
 }
 
-bool	tokenize(t_shell *shell)
+t_token_type	find_token(const char *cmd, size_t *len_out)
 {
-	t_token	current;
+	static const char	*strs[TOKENS_NB] = TOKENS_STR;
+	uint8_t				i;
 
-	(void) shell;
-	if (!token_init(&current, T_WORD))
-		return (false);
-	while (current.type != T_EOF)
+	i = 0;
+	*len_out = 0;
+	while (i < TOKENS_NB && ft_strncmp(cmd, strs[i], ft_strlen(strs[i])))
+		i++;
+	if (i < TOKENS_NB)
+		*len_out = ft_strlen(strs[i]);
+	return (i);
+}
+
+void	update_quote_context(t_lexer *lex)
+{
+	if (lex->quote == Q_NONE && lex->token == T_SQUOTE)
+		lex->quote = Q_SINGLE;
+	else if (lex->quote == Q_NONE && lex->token == T_DQUOTE)
+		lex->quote = Q_DOUBLE;
+	else if ((lex->token == T_SQUOTE && lex->quote == Q_SINGLE)
+			|| (lex->token == T_DQUOTE && lex->quote == Q_DOUBLE))
+		lex->quote = Q_NONE;
+}
+
+void	tokenize(t_collection *tokens, const char *cmd)
+{
+	t_lexer			lex;
+	size_t			len;
+	size_t			start;
+
+	lex.i = 0;
+	lex.quote = Q_NONE;
+	start = 0;
+	while(cmd[lex.i])
 	{
-		
+		lex.token = find_token(cmd + lex.i, &len);
+		if (lex.token != T_WORD)
+		{
+			if (start < lex.i)
+				collection_append(tokens, &((t_token){start, lex.i - start, T_WORD, lex.quote}));
+			collection_append(tokens, &((t_token){lex.i, len, lex.token, lex.quote}));
+			start = lex.i + len;
+		}
+		update_quote_context(&lex);
+		lex.i += len + !len;
 	}
-	return (true);
+	if (cmd[start])
+		collection_append(tokens, &((t_token){start, lex.i - start, T_WORD, lex.quote}));
 }
 
 void	__token_iter(void *ptr, void *arg)
 {
 	t_token *token;
+	char	*cmd = *(char **)arg;
 
-	(void) arg;
 	token = ptr;
 	if (token)
-		ft_printf("[%d : %s], ", token->type, (char *)(token->str.data));
+	{
+		ft_printf("[%d:{", token->type);
+		write(1, cmd + token->start, token->len);
+		ft_printf("}]\n");
+	}
 }
 
 int	main(int ac, char **av)
@@ -148,8 +153,8 @@ int	main(int ac, char **av)
 
 	if (ac != 2 || !shell_init(&shell, av[1]))
 		return (1);
-	tokenize(&shell);
-	collection_iter(&(shell.lexer.tokens), NULL, __token_iter);
+	tokenize(&shell.tokens, shell.cmd);
+	collection_iter(&(shell.tokens), &shell.cmd, __token_iter);
 	shell_destroy(&shell);
 	return (0);
 }
