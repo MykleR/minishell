632da9c4a70336fd47cb8dbe89b1f765108df559
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-02-12 13:47:35 +0100

    Edit PoC Builtin (Need to rework strchr x trim)

--- sources/proto/builtins.c
+++ sources/proto/builtins.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 14:35:25 by thomarna          #+#    #+#             */
-/*   Updated: 2025/02/11 17:49:08 by thomarna         ###   ########.fr       */
+/*   Updated: 2025/02/12 09:49:20 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "collection.h"
@@ -14,6 +14,7 @@
 #include "libft_string.h"
 #include "proto.h"
 #include <linux/limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -25,8 +26,6 @@ void	ft_pwd(void)
 	if (getcwd(pwd, PATH_MAX))
 		printf("%s\n", pwd);
 }
-
-// get_env > builtins env (Need fnct features to list hashmap)
 
 char	*get_key(char *src)
 {
@@ -40,7 +39,7 @@ char	*get_key(char *src)
 	if (!dest)
 		return (NULL);
 	ft_strlcpy(dest, src, i + 1);
-	return (dest);
+	return (ft_strdup(dest));
 }
 
 char	*get_value(char *src)
@@ -56,49 +55,29 @@ void	init_env(char **ep, t_collection *c)
 	hm_create(c, 2, sizeof(char **), (t_clear_info){alloc_f, T_HEAP});
 	while (*ep)
 	{
-		/* printf("%s=%s\n", get_key(*ep), get_value(*ep)); */
-		hm_set(c, get_key(*ep), get_value(*ep));
+		/* printf("%s %s\n", get_key(*ep), get_value(*ep)); */
+		hm_set(c, get_key(*ep), &((char *){ft_strdup(get_value(*ep))}));
 		ep++;
 	}
 }
 
+void	env_print(const char *key, t_collection *c)
+{
+	(void)c;
+	printf("%s=%s\n", key, (char *)hm_get(c, key));
+	printf("%s\n", key);
+}
 void	str_print(void *ptr, void *arg)
 {
 	(void)arg;
-	ft_printf("%s, ", *(char **)ptr);
+	ft_printf("%s %s\n", *(char **)ptr, *(char **)hm_get(arg, *(char **)ptr));
 }
 
-void	env_iter(t_hm *h, void *arg, void (*iter)(const char *, const char *,
-			void *))
+void	get_env(t_hm *h)
 {
-	uint32_t		i;
-	t_collection	*keys;
-	t_collection	*vals;
-	uint32_t		j;
-
-	i = 0;
-	j = 0;
-	if (!h || !h->data)
-		return ;
-	while (i < h->cap)
-	{
-		keys = &(((t_hm_bucket *)h->data) + i)->keys;
-		vals = &(((t_hm_bucket *)h->data) + i)->values;
-		j = 0;
-		while (j < keys->len)
-		{
-			iter(*((char **)keys->data + j), vals->data + vals->mem * j, arg);
-			j++;
-		}
-		i++;
-	}
-	return ;
-}
-
-void	get_env(const char *key, const char *value, void *arg)
-{
-	(void)arg;
-	printf("%s=%s\n", key, value);
+	t_collection *keys;
+	keys = &(((t_hm_bucket *)h->data))->keys;
+	collection_iter(keys, h, str_print);
 }
 
 int	main(int ac, char **av, char **ep)
@@ -107,9 +86,8 @@ int	main(int ac, char **av, char **ep)
 
 	init_env(ep, &c);
 	/* printf("%s\n", (char *)hm_get(&c, "PATH")); */
-	/* hm_set(&c, "PATH", ft_strdup("graou")); */
-	/* printf("%s\n", (char *)hm_get(&c, "PATH")); */
-	env_iter(&c, NULL, get_env);
+	/* printf("%s\n", *(char **)hm_get(&c, "LS_COLORS")); */
+	get_env(&c);
 	(void)ac;
 	(void)av;
 }
