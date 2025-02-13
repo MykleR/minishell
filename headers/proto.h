e2f9590fe1c88d6172661d8a128547f500567aa4
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-02-13 13:14:47 +0100

    Refacto PoC get_env (env), export_env (export)

--- headers/proto.h
+++ headers/proto.h
@@ -6,12 +6,13 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:00:04 by thomarna          #+#    #+#             */
-/*   Updated: 2025/01/20 16:07:49 by thomarna         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:10:08 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROTO_H
 # define PROTO_H
+# include "minishell.h"
 # include <errno.h>
 # include <fcntl.h>
 # include <stdio.h>
@@ -19,18 +20,7 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
-typedef struct s_env
-{
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}					t_env;
-
-typedef struct s_pipe
-{
-	char **env;
-	int	size;
-	int	outfile;
-}	t_pipe;
-
+void	init_env(char **ep, t_collection *c);
+char	*get_value(char *src);
+char	*get_key(char *src);
 #endif
--- sources/proto/builtins.c
+++ sources/proto/builtins.c
@@ -1,4 +1,4 @@
-
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
@@ -6,9 +6,10 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 14:35:25 by thomarna          #+#    #+#             */
-/*   Updated: 2025/02/12 09:49:20 by thomarna         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:14:04 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "collection.h"
 #include "libft.h"
 #include "libft_string.h"
@@ -19,6 +20,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+//Need to rework pwd (add protec)
 void	ft_pwd(void)
 {
 	char	pwd[PATH_MAX];
@@ -27,67 +29,10 @@ void	ft_pwd(void)
 		printf("%s\n", pwd);
 }
 
-char	*get_key(char *src)
+void	export_env(t_hm *h, char *key, char *value)
 {
-	int		i;
-	char	*dest;
-
-	i = 0;
-	while (src[i] && src[i] != '=')
-		i++;
-	dest = malloc(sizeof(char) * (i + 1));
-	if (!dest)
-		return (NULL);
-	ft_strlcpy(dest, src, i + 1);
-	return (ft_strdup(dest));
-}
-
-char	*get_value(char *src)
-{
-	char	*dest;
-
-	dest = ft_strtrim(ft_strrchr(src, '='), "=");
-	return (ft_strdup(dest));
-}
-
-void	init_env(char **ep, t_collection *c)
-{
-	hm_create(c, 2, sizeof(char **), (t_clear_info){alloc_f, T_HEAP});
-	while (*ep)
-	{
-		/* printf("%s %s\n", get_key(*ep), get_value(*ep)); */
-		hm_set(c, get_key(*ep), &((char *){ft_strdup(get_value(*ep))}));
-		ep++;
-	}
-}
-
-void	env_print(const char *key, t_collection *c)
-{
-	(void)c;
-	printf("%s=%s\n", key, (char *)hm_get(c, key));
-	printf("%s\n", key);
-}
-void	str_print(void *ptr, void *arg)
-{
-	(void)arg;
-	ft_printf("%s %s\n", *(char **)ptr, *(char **)hm_get(arg, *(char **)ptr));
-}
-
-void	get_env(t_hm *h)
-{
-	t_collection *keys;
-	keys = &(((t_hm_bucket *)h->data))->keys;
-	collection_iter(keys, h, str_print);
-}
-
-int	main(int ac, char **av, char **ep)
-{
-	t_collection	c;
-
-	init_env(ep, &c);
-	/* printf("%s\n", (char *)hm_get(&c, "PATH")); */
-	/* printf("%s\n", *(char **)hm_get(&c, "LS_COLORS")); */
-	get_env(&c);
-	(void)ac;
-	(void)av;
+	if (!value)
+		hm_set(h, key, &((char *){ft_strdup("")}));
+	else
+		hm_set(h, key, &((char *){ft_strdup(value)}));
 }
